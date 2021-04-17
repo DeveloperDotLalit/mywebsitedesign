8bd67ff31c9e0757e8b6a0f97fbc4a019de9d87c
theDLSBoy <lalits.dhurve5511@gmail.com>
2021-04-18 00:16:33 +0530

    kahi tri

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Layoutcontainer from './components/Layoutcontainer'
+function App() {
+  return (
+    <div className="App">
+      <Layoutcontainer/>
+    </div>
+  );
+}
+
+export default App;
